33384ce2b7dea4f3b72c4add789b26a913810f93
Jonathan Schwartz <jonbs98@gmail.com>
2020-06-30 21:56:53 -0400

    Made my way to starting question 7, q5 still needs some work

--- hw2.py
+++ hw2.py
@@ -1,11 +1,13 @@
 import numpy as np
-
+import math as m
+from cmath import sqrt
 
 def log_func(x):
     result = (np.log(x) / np.log(6)) ** 1.5
     return result
 
 def divided_difference_coeff(vals):
+    #print("vals:", vals)
     if len(vals) % 2 != 0:
         print("Error! input must be an even length!")
         print(vals)
@@ -16,6 +18,7 @@ def divided_difference_coeff(vals):
 
         elif len(vals) == 4:
             returnVal = (vals[3] - vals[2]) / (vals[1] - vals[0])
+            #print("Output:", returnVal)
             return returnVal
         else:
             middle = len(vals) // 2
@@ -25,11 +28,9 @@ def divided_difference_coeff(vals):
             vals_left = np.concatenate([x_vals[:-1], f_vals[:-1]])
             vals_right = np.concatenate([x_vals[1:], f_vals[1:]])
 
-            #print("left:\n", vals_left)
-            #print("right:\n", vals_right)
-
-
-            return (divided_difference_coeff(vals_right) - divided_difference_coeff(vals_left)) / (x_vals[-1] - x_vals[0])
+            output = (divided_difference_coeff(vals_right) - divided_difference_coeff(vals_left)) / (x_vals[-1] - x_vals[0])
+            #print("output:", output)
+            return output
 
 def interpolation(vals, x_interp):
     middle = len(vals) // 2
@@ -150,38 +151,115 @@ print("or less, which means it should be accurate to 6 decimal places.")
 
 print("\n\n\tPROBLEM 3\n========================")
 def newton_method_tan(x_val):
-    val = np.tan(x_val)
-    deriv = 1 / np.cos(x_val)**2
+    val = x_val - np.tan(x_val)
+    deriv = 1 - (1 / np.cos(x_val))**2
 
-    next_x_val = x_val + (val / deriv)
+    #print("X:\t", x_val, "f(x):\t", val, "f'(x):\t", deriv)
 
+    next_x_val = x_val - (val / deriv)
     return next_x_val
 
 
-x_low = 10.5
-counter = 0
-while counter < 10:
-    x_low = newton_method_tan(x_low)
-    counter += 1
+x_vals = np.arange(7,15,0.1)
+for x_val in x_vals:
+    counter = 0
+    start = x_val
+    while counter < 10:
+        x_val = newton_method_tan(x_val)
+        counter += 1
 
-print("Left x boundary for tan(x) when approaching 11:\t\t", x_low)
+    if start == x_vals[0]:
+        solutions = [x_val]
+    else:
+        solutions.append(x_val)
 
-x_high = 11.5
-counter = 0
-while counter < 10:
-    x_high = newton_method_tan(x_high)
-    counter += 1
+print("Left x boundary for tan(x) root near 11:\t", solutions[10])
+print("Right x boundary for tan(x) root near 11:\t", solutions[15])
 
-print("Right x boundary for tan(x) when approaching 11:\t", x_high)
+#aaa = np.array([1,2,3,4,5,6])
+#b = np.concatenate([aaa[:2], aaa[-2:]])
+#print(b)
 
-
-
-
-print("\n\n\tPROBLEM 4\n========================")
-print("skipping this for now, will return using pages 20-24 of nonlinear equation roots notes")
 
 
 print("\n\n\tPROBLEM 5\n========================")
+def MullerMethod(guesses, f, iter = 5000):
+    counter = 0
+    res = 0.0
+    while counter < iter:
+        #print("guesses: ", guesses)
+        guess_sols = [ f(guesses[0]) ]
+        for g in guesses[1:]:
+            guess_sols.append( f(g) )
 
+        f1 = f(guesses[0]); f2 = f(guesses[1]); f3 = f(guesses[2]);
+        d1 = f1 - f3;
+        d2 = f2 - f3;
+        h1 = guesses[0] - guesses[2];
+        h2 = guesses[1] - guesses[2];
+        a0 = f3;
+        a1 = (((d2 * pow(h1, 2)) -
+               (d1 * pow(h2, 2))) /
+              ((h1 * h2) * (h1 - h2)));
+        a2 = (((d1 * h2) - (d2 * h1)) /
+              ((h1 * h2) * (h1 - h2)));
+
+        x1 = ((-2 * a0) / (a1 + abs(sqrt(a1 * a1 - 4 * a0 * a2))));
+        x2 = ((-2 * a0) / (a1 -  abs(sqrt(a1 * a1 - 4 * a0 * a2))));
+
+
+        if (x1 >= x2):
+            res = x1 + guesses[2]
+        else:
+            res = x2 + guesses[2]
+
+        if abs(res - guesses[2]) < 1e-4:
+            res = round(res, 6)
+            print("Root found at x =", res)
+            return res
+        else:
+            guesses[0] = guesses[1]
+            guesses[1] = guesses[2]
+            guesses[2] = res
+            counter+=1
+            #print("resid: ", res)
+
+    print("Muller method didn't work; exceeded max iterations :( ")
+    return guesses[2]
+
+def f_q5(x):
+    return x**3 - 4*x**2 + 6*x - 4
+
+def f_q5_only_complex(x):
+    #roots: x = 1 +/- j
+    return x**2 - 2*x + 2
+
+def f_q5_no_complex(x):
+    # roots: x = [1, 2.5, 10]
+    # very easy to converge to 2.5 and 10, 1 is very difficult to get
+    return x**3 - 13.5*x**2 + 37.5*x - 25
+
+#sol_q5 = MullerMethod(np.array([0.7, 0.8, 0.9]), f_q5_no_complex)
+# had trouble getting the algorithm to recognize complex roots, kinda works
+# though so I'm going to leave it for now
+sol_q5 = MullerMethod(np.array([0.5+0.5j, 0.75+0.75j, 0.99+0.99j]), f_q5)
+print(sol_q5)
+
+print("\n\n\tPROBLEM 6\n========================")
+A = np.array([ [1, -9, 26, 0],
+               [0, 1, -9, 26],
+               [0, 1, 3, -10],
+               [0, 0, 1, 3] ])
+aDet = np.linalg.det(A)
+
+print("These equations share a root: ", aDet == 0)
+
+print("\n\n\tPROBLEM 7\n========================")
+
+def p(x, y):
+    return 2*x**2 + 2*y**2 - 1
+
+def q(x, y):
+    return x**2 + y**2 + 2*x*y - x + y
 
 print("\n\n")
