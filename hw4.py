04d03170c30bfa3597a15fafbd0ed4feede2e379
Jonathan Schwartz <jonbs98@gmail.com>
2020-07-14 16:55:50 -0400

    Figured out 1c, I did RK4 backwards but I think that's okay

--- hw4.py
+++ hw4.py
@@ -14,10 +14,29 @@ for i in reversed(x1[:-1]):
     last = last - 0.05 * slope
     y1b = np.concatenate(([last], y1b))
 
+def rk4_step(x, y, h, f):
+    k1 = h*f(y)
+    k2 = h*f(y - k1/2)
+    k3 = h*f(y - k2/2)
+    k4 = h*f(y - k3)
 
+    y_new = y - (k1 + 2*k2 + 2*k3 + k4) / 6
+
+    return y_new
+
+def rhs_1c(y):
+    return 1 / (3 * y**2)
+
+y1c = np.array([1])
+
+for x in reversed(x1[:-1]):
+    new_y = rk4_step(x, y1c[0], 0.05, rhs_1c)
+    print(new_y)
+    y1c = np.concatenate(([new_y], y1c))
 
 plt.plot(x1, y1a, label='Analytical')
 plt.plot(x1, y1b, label='Euler Estimate', linestyle='--')
+plt.plot(x1, y1c, label='Runge-Kutta 4 Approx', linestyle='--')
 plt.xlim(1,2)
 plt.title("Problem 1 - Estimating Differential Equation")
 plt.xlabel("X")
