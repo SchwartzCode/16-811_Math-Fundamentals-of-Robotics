d3db51d47f8ab1001ae710ed86f3656ad298272e
Jonathan Schwartz <jonbs98@gmail.com>
2020-07-23 14:57:51 -0400

    Generates a few random blobs to generate convex shells for, next up is to add the robot and have it navigate aroudn the blobs

--- hw6.py
+++ hw6.py
@@ -5,23 +5,20 @@ from matplotlib import pyplot as plt
 
 #points = np.array([[0,0], [1,1], [1,0], [0,1], [0.5,1.5], [0.5,0.5]])
 
-points = np.array([[0,0]])
-for i in range(49):
-    newPoint = np.array([[-1+2*random.random(), -1+2*random.random()]])
-    points = np.vstack([points, newPoint])
+points1 = np.array([[2+2*random.random(), 1+2*random.random()]])
+for i in range(19):
+    newPoint = np.array([[2+2*random.random(), 1+2*random.random()]])
+    points1 = np.vstack([points1, newPoint])
 
+points2 = np.array([[1+2*random.random(), 4+2*random.random()]])
+for i in range(19):
+    newPoint = np.array([[1+2*random.random(), 4+2*random.random()]])
+    points2 = np.vstack([points2, newPoint])
 
-leftDex = 4
-for i in range(len(points[:,0])):
-    if i == leftDex:
-        print("heeple")
-    elif points[i,0] < points[leftDex,0]:
-        #if point is further left than current min
-        leftDex = i
-    elif points[i,0] == points[leftDex, 0]:
-        if points[i,1] < points[leftDex,1]:
-            #if point is as left and lower than current min
-            leftDex = i
+points3 = np.array([[4+2*random.random(), 4+2*random.random()]])
+for i in range(19):
+    newPoint = np.array([[4+2*random.random(), 4+2*random.random()]])
+    points3 = np.vstack([points3, newPoint])
 
 def getNextHullPoint(hullDexes, points, firstPoint=False):
     maxAngle = -1 #set starting point lower than any real point would be
@@ -53,31 +50,57 @@ def getAngle(point1, point2, point3):
     theta = np.arccos(dotProd / (mag1*mag2))
     return theta
 
+def getConvexPoly(points):
+    leftDex = 4
+    for i in range(len(points[:,0])):
+        if i == leftDex:
+            print("heeple")
+        elif points[i,0] < points[leftDex,0]:
+            #if point is further left than current min
+            leftDex = i
+        elif points[i,0] == points[leftDex, 0]:
+            if points[i,1] < points[leftDex,1]:
+                #if point is as left and lower than current min
+                leftDex = i
 
-currDex = leftDex
-hullPoints = np.array([ leftDex ])
+    currDex = leftDex
+    hullPoints = np.array([ leftDex ])
 
-currDex = getNextHullPoint(hullPoints, points, firstPoint=True)
-hullPoints = np.append(hullPoints, [currDex])
-
-while (currDex != leftDex):
-    currDex = getNextHullPoint(hullPoints, points)
+    currDex = getNextHullPoint(hullPoints, points, firstPoint=True)
     hullPoints = np.append(hullPoints, [currDex])
 
-x_vals = [ ]
-y_vals = [ ]
+    while (currDex != leftDex):
+        currDex = getNextHullPoint(hullPoints, points)
+        hullPoints = np.append(hullPoints, [currDex])
 
-for i in hullPoints:
-    x_vals.append(points[i,0])
-    y_vals.append(points[i,1])
+    x_vals = [ ]
+    y_vals = [ ]
 
+    for i in hullPoints:
+        x_vals.append(points[i,0])
+        y_vals.append(points[i,1])
+
+    return leftDex, x_vals, y_vals
+
+leftDex1, x1, y1 = getConvexPoly(points1)
+leftDex2, x2, y2 = getConvexPoly(points2)
+leftDex3, x3, y3 = getConvexPoly(points3)
 
 plt.title("Convex Hull Demonstration")
-plt.scatter(points[:,0], points[:,1], label='Points')
+plt.scatter(points1[:,0], points1[:,1], color='blue', label='Points')
+plt.scatter(points2[:,0], points2[:,1], color='blue')
+plt.scatter(points3[:,0], points3[:,1], color='blue')
 plt.xlabel("X")
 plt.ylabel("Y")
-plt.scatter(points[leftDex,0], points[leftDex,1], color='purple', label='Left Point')
-plt.legend(loc='upper right')
+plt.scatter(points1[leftDex1,0], points1[leftDex1,1], color='purple', label='Left Point')
+plt.scatter(points2[leftDex2,0], points2[leftDex2,1], color='purple')
+plt.scatter(points3[leftDex3,0], points3[leftDex3,1], color='purple')
 
-plt.plot(x_vals, y_vals, color='green', linestyle='--', label='Convex Hull')
+plt.plot(x1, y1, color='green', linestyle='--', label='Convex Hull')
+plt.plot(x2, y2, color='green', linestyle='--')
+plt.plot(x3, y3, color='green', linestyle='--')
+
+plt.xlim(0,8)
+plt.ylim(0,8)
+plt.legend(loc='upper right')
 plt.show()
