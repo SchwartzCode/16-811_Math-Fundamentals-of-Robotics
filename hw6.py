c1da00164ac4bcdb5f1e7756e505058e3f6ba336
Jonathan Schwartz <jonbs98@gmail.com>
2020-07-26 20:32:34 -0400

    Got a basic algorithm moving although it doesn't totally work, will keep at it

--- hw6.py
+++ hw6.py
@@ -9,6 +9,7 @@ class blob(object):
     points = np.array([1])
     leftDex = np.array([-1,-1])
     hullPts = np.array([])
+    center = np.array([0.0, 0.0])
 
     def __init__(self, x_cent, y_cent, width, point_count=20):
 
@@ -19,6 +20,7 @@ class blob(object):
 
         leftDex, hullx, hully = self.getConvexPoly(self.points)
         self.hullPts = np.transpose( np.vstack([hullx, hully]) )
+        self.genCoM()
 
 
 
@@ -80,25 +82,140 @@ class blob(object):
 
         return leftDex, x_vals, y_vals
 
+    def genCoM(self):
+        xSum = 0.0
+        ySum = 0.0
+        ptCount = len(self.points)
+
+        for i in range(ptCount):
+            xSum += self.points[i,0]
+            ySum += self.points[i,1]
+
+        self.center = np.array([ (xSum/ptCount), (ySum/ptCount) ])
+
+    def onSegment(self, p, q, r):
+        # Taken from geeksforgeeks.com:
+        # https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+        if ( (q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and (q[1] <= max(p[1], r[1])) and (q[1] >= min(p[1], r[1]))):
+            return True
+        return False
+
+    def orientation(self, p, q, r):
+        # Taken from geeksforgeeks.com:
+        # https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+
+        # to find the orientation of an ordered triplet (p,q,r)
+        # function returns the following values:
+        # 0 : Colinear points
+        # 1 : Clockwise points
+        # 2 : Counterclockwise
+
+        # See https://www.geeksforgeeks.org/orientation-3-ordered-points/amp/
+        # for details of below formula.
+
+        val = (float(q[1] - p[1]) * (r[0] - q[0])) - (float(q[0] - p[0]) * (r[1] - q[1]))
+        if (val > 0):
+
+            # Clockwise orientation
+            return 1
+        elif (val < 0):
+
+            # Counterclockwise orientation
+            return 2
+        else:
+
+            # Colinear orientation
+            return 0
+
+    # The main function that returns true if
+    # the line segment 'p1q1' and 'p2q2' intersect.
+    def doIntersect(self, p1,q1,p2,q2):
+        # Taken from geeksforgeeks.com:
+        # https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+
+        # Find the 4 orientations required for
+        # the general and special cases
+        o1 = self.orientation(p1, q1, p2)
+        o2 = self.orientation(p1, q1, q2)
+        o3 = self.orientation(p2, q2, p1)
+        o4 = self.orientation(p2, q2, q1)
+
+        # General case
+        if ((o1 != o2) and (o3 != o4)):
+            return True
+
+        # Special Cases
+
+        # p1 , q1 and p2 are colinear and p2 lies on segment p1q1
+        if ((o1 == 0) and self.onSegment(p1, p2, q1)):
+            return True
+
+        # p1 , q1 and q2 are colinear and q2 lies on segment p1q1
+        if ((o2 == 0) and self.onSegment(p1, q2, q1)):
+            return True
+
+        # p2 , q2 and p1 are colinear and p1 lies on segment p2q2
+        if ((o3 == 0) and self.onSegment(p2, p1, q2)):
+            return True
+
+        # p2 , q2 and q1 are colinear and q1 lies on segment p2q2
+        if ((o4 == 0) and self.onSegment(p2, q1, q2)):
+            return True
+
+        # If none of the cases
+        return False
+
+    def getIntersection(self, p1, q1, p2, q2):
+        # Used logic form a stack overflow answer:
+        # https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
+        m1 = (q1[1] - p1[1]) / (q1[0] - p1[0])
+        m2 = (q2[1] - p2[1]) / (q2[0] - p2[0])
+        b1 = p1[1] - m1*p1[0]
+        b2 = p2[1] - m2*p2[0]
+
+        x_int = (b2 - b1) / (m1 - m2)
+        y_int = m1*x_int + b1
+
+        inv_slope = -1 / m2
+
+        return np.array([x_int, y_int]), inv_slope
+
+    def detectIntersection(self, pathPts):
+        for i in range(1, len(pathPts)):
+            #loop through all path points
+            pt1 = pathPts[i-1]
+            pt2 = pathPts[i]
+
+            for j in range(1,len(self.hullPts)):
+                #loop through all line segments that form object boudnary
+                start = self.hullPts[j-1]
+                end = self.hullPts[j]
+
+                if self.doIntersect(start, end, pt1, pt2):
+                    #generate new path point to avoid intersection
+                    intersection, iSlope = self.getIntersection(start, end, pt1, pt2)
+                    #print(intersection)
+
+                    poss_pt1 = np.array([ (intersection[0] + 0.5), (intersection[1] + 0.5*iSlope) ])
+                    poss_pt2 = np.array([ (intersection[0] - 0.5), (intersection[1] - 0.5*iSlope) ])
+
+                    dist1 = np.sqrt( (poss_pt1[0] - self.center[0])**2 + (poss_pt1[1] - self.center[1])**2 )
+                    dist2 = np.sqrt( (poss_pt2[0] - self.center[0])**2 + (poss_pt2[1] - self.center[1])**2 )
+
+                    if dist1 > dist2:
+
+                        pathPts = np.vstack([ pathPts[:i], poss_pt1, pathPts[i:] ])
+                    else:
+
+                        pathPts = np.vstack([ pathPts[:i], poss_pt2, pathPts[i:] ])
+
+                    #i=1
+                    #j=1
+        return pathPts
 
 
 
-points1 = np.array([[2+2*random.random(), 1+2*random.random()]])
-points2 = np.array([[1+2*random.random(), 4+2*random.random()]])
-points3 = np.array([[4+2*random.random(), 4+2*random.random()]])
-for i in range(19):
-    newPoint1 = np.array([[2+2*random.random(), 1+2*random.random()]])
-    points1 = np.vstack([points1, newPoint1])
-    newPoint2 = np.array([[1+2*random.random(), 4+2*random.random()]])
-    points2 = np.vstack([points2, newPoint2])
-    newPoint3 = np.array([[4+2*random.random(), 4+2*random.random()]])
-    points3 = np.vstack([points3, newPoint3])
 
-
-
-
-def genCoM():
-    print("aa")
 
 
 
@@ -107,6 +224,11 @@ finishPt = np.array([8,8])
 
 pathPts = np.array([startPt, finishPt])
 obstructed = False
+
+obj1 = blob(2, 1, 2)
+obj2 = blob(5 ,2, 1.5)
+obj3 = blob(2, 5, 2)
+obj4 = blob(6, 5, 2)
 
 """
 if (pathPts): #check for obstruction
@@ -119,24 +241,31 @@ while(obstructed and iter < 1e3):
     # check again to see if obstructed, set obstructed variable accoringly
 """
 
-obj1 = blob(2, 1, 2)
-obj2 = blob(5 ,2, 1.5)
-obj3 = blob(2, 5, 2)
-obj4 = blob(6, 5, 2)
-
+print(pathPts)
+pathPts = obj1.detectIntersection(pathPts)
+print(pathPts)
+#pathPts = obj2.detectIntersection(pathPts)
+#pathPts = obj3.detectIntersection(pathPts)
+pathPts = obj4.detectIntersection(pathPts)
+print(pathPts)
 
 plt.title("Convex Hull Demonstration")
 plt.xlabel("X [m]")
 plt.ylabel("Y [m]")
 plt.scatter(obj1.points[:,0], obj1.points[:,1], color='blue', label='Points')
-plt.scatter(obj2.points[:,0], obj2.points[:,1], color='blue', label='Points')
-plt.scatter(obj3.points[:,0], obj3.points[:,1], color='blue', label='Points')
-plt.scatter(obj4.points[:,0], obj4.points[:,1], color='blue', label='Points')
+plt.scatter(obj2.points[:,0], obj2.points[:,1], color='blue')
+plt.scatter(obj3.points[:,0], obj3.points[:,1], color='blue')
+plt.scatter(obj4.points[:,0], obj4.points[:,1], color='blue')
+
+#plt.scatter(obj1.center[0], obj1.center[1], color='purple')
+#plt.scatter(obj2.center[0], obj2.center[1], color='purple')
+#plt.scatter(obj3.center[0], obj3.center[1], color='purple')
+#plt.scatter(obj4.center[0], obj4.center[1], color='purple')
 
 plt.plot(obj1.hullPts[:,0], obj1.hullPts[:,1], color='green', linestyle='--', label='Convex Hull')
-plt.plot(obj2.hullPts[:,0], obj2.hullPts[:,1], color='green', linestyle='--', label='Convex Hull')
-plt.plot(obj3.hullPts[:,0], obj3.hullPts[:,1], color='green', linestyle='--', label='Convex Hull')
-plt.plot(obj4.hullPts[:,0], obj4.hullPts[:,1], color='green', linestyle='--', label='Convex Hull')
+plt.plot(obj2.hullPts[:,0], obj2.hullPts[:,1], color='green', linestyle='--')
+plt.plot(obj3.hullPts[:,0], obj3.hullPts[:,1], color='green', linestyle='--')
+plt.plot(obj4.hullPts[:,0], obj4.hullPts[:,1], color='green', linestyle='--')
 
 plt.scatter(startPt[0],startPt[1], color='red', label='Start')
 plt.scatter(finishPt[0], finishPt[1], color='orange', label='Finish')
